b4da9cfcf4a4c200c7ef978d3af25b0680614d14
Satrirat Seewan <115156411+satrirattt@users.noreply.github.com>
2023-07-10 18:38:52 +0700

    Add files via upload

---
+++ C.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{	
+	cout<<"  \x23\x23\x23\x23 \n";
+	cout<<" \x23\x23  \x23\x23\n";
+	cout<<"\x23\x23 \n";
+	cout<<"\x23\x23 \n";
+	cout<<" \x23\x23  \x23\x23 \n";
+	cout<<"  \x23\x23\x23\x23 \n";
+	
+	system("pause");
+	return 0;
+}

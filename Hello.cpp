952c0f40d01c1af3ec6e421f1804173a8c776868
Satrirat Seewan <115156411+satrirattt@users.noreply.github.com>
2023-07-10 10:39:09 +0700

    Add files via upload

---
+++ Hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{	cout<<"Student Code : 6606021620198"<<endl;
+	cout<<"Student Name : Satrirat Seewan"<<endl;
+	cout<<"Division : Information Technology"<<endl;
+	cout<<"Year : 1"<<endl;
+	cout<<"Room : RA"<<endl;
+	cout<<"Sex : Female"<<endl;
+	cout<<"Age : 18"<<endl;
+	system("pause");
+	return 0;
+}

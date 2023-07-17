cabe6f836f8706766016a0d336ad186e77e4419e
Satrirat Seewan <115156411+satrirattt@users.noreply.github.com>
2023-07-17 10:45:32 +0700

    Add files via upload

--- Ex3.cpp
+++ Ex3.cpp
@@ -10,7 +10,7 @@ int main()
     cin >> Name ;
     cout << "Enter Weight : " ;
     cin >>  W ;
-    cout << "Enter Hight : " ;
+    cout << "Enter Height : " ;
     cin >>  H;
     cout << "BMI = " << W / (H/100 * H/100) <<endl ;
 

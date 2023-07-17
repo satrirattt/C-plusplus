54e10dc1b141a2c46c3d842ffc011e1c05345cd7
Satrirat Seewan <115156411+satrirattt@users.noreply.github.com>
2023-07-17 09:54:58 +0700

    Add files via upload

---
+++ Ex2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{	
+	string Name;
+	int Year;
+	cout << "Enter Name : " ;
+	cin >> Name ;
+	cout << "Enter Year of  birth : " ;
+	cin >> Year ;
+	cout << "\nYour Age = " << 2566 - Year << " years old. " <<endl ;
+
+	
+	return (0);
+
+}

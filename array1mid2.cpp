41d16c998aee5f8ed1edb405a781ba8cd015db67
Satrirat Seewan <115156411+satrirattt@users.noreply.github.com>
2023-09-18 11:03:56 +0700

    Update array1mid2.cpp

--- array1mid2.cpp
+++ array1mid2.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 int main()
 {
-	int num[4], total = 0, avg = 0;
+	int num[4], total = 0, avg = 0,max = 0;
 	string name[4];
 	for (int i = 0; i < 4; i++)//i<=3 input array
 	{	cout<<"Enter name ["<<i<<"] : " ;
@@ -20,8 +20,11 @@ int main()
 	for (int i = 0; i < 4; i++)//i<=3 display array
 	{	cout <<" " << name[i] <<"\t\t"<<num[i] <<endl;
 		total = total + num[i];
+		if(num[i] > max)
+			max = num[i];
 	}
 	cout << "---------------------"<<endl;//setfill('-')<<setw(30)
+	cout<<"Max score = " <<max<<endl;
 	cout<<"Total = " <<total<<endl;
 	cout<<"Average = "<<(float)total/4<<endl;
 	

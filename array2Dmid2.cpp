b90a3bf92fd2db75880684db7b269fa47051804e
Satrirat Seewan <115156411+satrirattt@users.noreply.github.com>
2023-10-02 11:53:15 +0700

    Create array2Dmid2.cpp

---
+++ array2Dmid2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+using namespace std;
+void display(int num[3][3],string name[3]);
+int main()
+{
+	int num[3][3];
+	string name[3];
+	for(int i = 0; i < 3;i++)
+	{
+		cout << "Student " <<i+1<<endl;
+		cout << "Name ["<<i+1<<"] : " ;
+		cin >> name[i];
+		for(int j = 0; j < 3;j++)
+		{
+			cout << "Score ["<<i<<"]["<<j<<"] : " ;
+			cin >> num[i][j];
+		}
+	}
+	display(num,name);
+	
+	system("pause");
+	return(0);
+}
+void display(int num[3][3],string name[3])
+{
+	cout << "Show Scores" <<endl;
+	cout << "----------------------------------" <<endl;
+	cout << " Student  Math   Science  English" <<endl;
+	cout << "----------------------------------" <<endl;
+	for(int i = 0; i < 3;i++ )
+	{
+		cout<<" "<<i+1<<" "<<name[i]<<"\t";
+		for(int j = 0;j < 3;j++ )
+		{
+			cout<<"   "<<num[i][j]<<"\t";
+
+		}
+		cout <<endl;
+	}
+}
